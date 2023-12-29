bac3d25fad35dc921fba1c6e8ec6ee056a536971
Gokul Raja <106546785+Gokul-Raja84@users.noreply.github.com>
2023-12-29 13:01:17 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/react-resume/",
+  base: "/My-Resume/",
   plugins: [react()],
 });
